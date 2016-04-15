d2822fc21f9f20cf4023ad49625c1e0a590d8e7a
wizz92 <wizz92@gmail.com>
2016-04-15 20:58:29 +0300

    removed Request dependency

--- src/Helpers/request.php
+++ src/Helpers/request.php
@@ -4,11 +4,23 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 function apiRequestProxy(Request $request)
 {
-    $method = $request->method();
-    $root = $request->root();
-    $requestString = $request->fullUrl();
-    $requestString = str_replace($root.config('api_configs.url'), '', $requestString);
+    $method = $_SERVER['REQUEST_METHOD'];
+    // $method = $request->method();
+    // $root = $request->root();
+    $root = $_SERVER['HTTP_HOST'];
+    // dd($root);
+    // dd($_SERVER);
+
+    $requestString = $_SERVER['PATH_INFO'];
+    // $requestString = $request->fullUrl();
+    // dd($requestString);
+    $requestString = str_replace(config('api_configs.url'), '', $requestString);
     $query = config('api_configs.secret_url').$requestString;
+    dd($_GET);
+    if ($_GET) {
+        # code...
+    }
+    // dd($query);
     session_write_close();
     $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_URL, $query); 
@@ -16,13 +28,13 @@ function apiRequestProxy(Request $request)
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_HEADER, true); 
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method); 
-    curl_setopt($ch, CURLOPT_COOKIE, getCookieStringFromRequest($request));
+    curl_setopt($ch, CURLOPT_COOKIE, $_SERVER['HTTP_COOKIE']);
+    // curl_setopt($ch, CURLOPT_COOKIE, getCookieStringFromRequest($request));
     if (in_array($method, ["PUT", "POST", "DELETE"])) 
     {
         $data = $request->all();
         if (array_get($data, 'files')) 
         {
-        
             $files = array_pull($data, 'files');
             $files = array_sign($files);
             foreach ($files as $key => $file) 
@@ -37,15 +49,12 @@ function apiRequestProxy(Request $request)
             }
             $data['files'] = $files;
         }
+        // TODO add some 
+        $data['ip'] = $_SERVER['REMOTE_ADDR'];
+        // $data['ip'] = $_SERVER['REMOTE_ADDR'];
         $data = ($method == "POST") ? array_sign($data) : http_build_query($data);
-        // if ($method == "POST") 
-        // {
-            // $data = array_sign($data);
+        // dd($data);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        // } else 
-        // {
-            // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-        // }
     }
     $res = curl_exec($ch);
     curl_close($ch);
--- src/routes.php
+++ src/routes.php
@@ -12,15 +12,17 @@ Route::get('/r/{slug?}', function(Request $request, $slug)
 
 Route::any('api/{slug?}', function($slug, Request $request) 
 {
-	$method = $request->method();
+	// $method = $request->method();
+	$method = $_SERVER['REQUEST_METHOD'];
 	$res = apiRequestProxy($request);
+	// dd($method);
 	
 	$data = explode("\r\n\r\n", $res);
 	$headers = (count($data) == 3) ? $data[1] : $data[0];
 	$res = (count($data) == 3) ? $data[2] : $data[1];
     $cookies = setCookiesFromCurlResponse($headers);
 
-	if (strpos('qwe'.$slug, 'download')) 
+	if (strpos('q'.$slug, 'download')) 
 	{
 		$headers = http_parse_headers($headers);
 		$filename = array_get($headers[0], 'content-disposition');
@@ -31,8 +33,7 @@ Route::any('api/{slug?}', function($slug, Request $request)
             file_put_contents(public_path().'/files/'.$filename, $res);
 			return response()->download(public_path().'/files/'.$filename);
         }
-	}
-	if (strpos('qwe'.$slug, 'documents')) 
+	} elseif (strpos('q'.$slug, 'documents')) 
 	{
 		$chunks = explode('/', $slug);
 		$filename = array_get($chunks, count($chunks) - 1);
@@ -41,12 +42,11 @@ Route::any('api/{slug?}', function($slug, Request $request)
             file_put_contents(public_path().'/documents/'.$filename, $res);
 			return response()->download(public_path().'/documents/'.$filename);
         }
-	}
-	if (strpos('qwe'.$slug, 'actions/print') || strpos('qwe'.$slug, 'superstats')) 
+	} elseif (strpos('q'.$slug, 'actions/print') || strpos('q'.$slug, 'superstats')) 
 	{
 		return $res;
-	}
-	if (strpos('qwe'.$res, 'Whoops,')) {
+	} elseif (strpos('q'.$res, 'Whoops,')) 
+	{
 		if (! json_decode($res)) {
 			return response()->json([
 				'status' => 400,
