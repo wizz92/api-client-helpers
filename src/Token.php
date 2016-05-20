2b743403ba1b2b148fa9ebd5dfd8185fa8fc1b79
wizz92 <wizz92@gmail.com>
2016-05-20 10:45:29 +0300

    test with cache

--- src/Token.php
+++ src/Token.php
@@ -1,6 +1,7 @@
 <?php 
 namespace Wizz\ApiClientHelpers;
 use \Illuminate\Http\Request;
+use \Cache;
 
 class Token
 {
@@ -31,26 +32,36 @@ class Token
 
     protected function getFromBootstrap($query)
     {   
-        $addition = array_get($_SERVER, 'QUERY_STRING', '');
-        $query .= ($addition) ? '&'.$addition : '';
-        // $cookie_string = getCookieStringFromRequest(request());
-        
-        // session_write_close();
-        $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, $query); 
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        
-        // curl_setopt($ch, CURLOPT_HEADER, true); 
-        // curl_setopt($ch, CURLOPT_COOKIE, $cookie_string);
-        $res = curl_exec($ch); 
-        curl_close($ch);
+        $cache_key = 'bootstrap_data_from_api';
+        if (Cache::has($cache_key)) 
+        {
+            $output = cache($cache_key);
+        } else
+        {
 
-        // $data = explode("\r\n\r\n", $res);
-        // $headers = (count($data) == 3) ? $data[1] : $data[0];
-        // $res = (count($data) == 3) ? $data[2] : $data[1];
-        // $cookies = setCookiesFromCurlResponse($headers);
-        $output = json_decode($res);
+            $addition = array_get($_SERVER, 'QUERY_STRING', '');
+            $query .= ($addition) ? '&'.$addition : '';
+            $cookie_string = getCookieStringFromRequest(request());
+            
+            session_write_close();
+            $ch = curl_init(); 
+            curl_setopt($ch, CURLOPT_URL, $query); 
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HEADER, true); 
+            curl_setopt($ch, CURLOPT_COOKIE, $cookie_string);
+            $res = curl_exec($ch); 
+            curl_close($ch);
+
+            $data = explode("\r\n\r\n", $res);
+            $headers = (count($data) == 3) ? $data[1] : $data[0];
+            $res = (count($data) == 3) ? $data[2] : $data[1];
+            $cookies = setCookiesFromCurlResponse($headers);
+
+            $output = json_decode($res);
+            Cache::put($cache_key, $output, 100);
+            
+        }
         if(!is_object($output))
         {
             $this->errors = $res;
