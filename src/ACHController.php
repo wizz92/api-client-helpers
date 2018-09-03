6531d6b97914097618e8153b219408e598ad7a9a
Rex <danielrexler@gmail.com>
2018-09-03 17:45:50 +0300

    working

--- src/ACHController.php
+++ src/ACHController.php
@@ -187,7 +187,7 @@ class ACHController extends Controller
               ->header('Content-Type', $r->content_type)
               ->header('Content-Disposition', array_get($r->headers, 'content-disposition'));
 
-        if (strpos($path, 'assets') !== false) {
+        if (strpos($request->path(), 'assets') !== false) {
           $response = $response->header('Cache-Control', "max-age=7776000");
         }
 
