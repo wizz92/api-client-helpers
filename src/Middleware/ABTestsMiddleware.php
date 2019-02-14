3d45033831ccdff2b20d4793a88e2306ce14e25c
Rex <danielrexler@gmail.com>
2019-02-14 16:01:48 +0200

    price increase experiment ready

--- src/Middleware/ABTestsMiddleware.php
+++ src/Middleware/ABTestsMiddleware.php
@@ -4,14 +4,21 @@ namespace Wizz\ApiClientHelpers\Middleware;
 
 use Wizz\ApiClientHelpers\Services\Getters\ClientConfigGetter;
 use Wizz\ApiClientHelpers\Services\Experiments\DefaultAcademicLevel\DefaultAcademicLevelExperiment;
+use Wizz\ApiClientHelpers\Services\Experiments\PriceIncrease\PriceIncreaseExperiment;
 use Wizz\ApiClientHelpers\Helpers\CacheHelper;
+use Wizz\ApiClientHelpers\Helpers\CookieHelper;
 use Closure;
+use Cookie;
 
 class ABTestsMiddleware
 {
-    public function __construct(DefaultAcademicLevelExperiment $defaultAcademicLevelExperiment)
+    public function __construct(
+      DefaultAcademicLevelExperiment $defaultAcademicLevelExperiment,
+      PriceIncreaseExperiment $priceIncreaseExperiment
+    )
     {
       $this->defaultAcademicLevelExperiment = $defaultAcademicLevelExperiment;
+      $this->priceIncreaseExperiment = $priceIncreaseExperiment;
       $app_id = CacheHelper::conf('client_id');
       $this->clientConfigGetter = new ClientConfigGetter($app_id);
     }
@@ -24,30 +31,61 @@ class ABTestsMiddleware
      */
     public function handle($request, Closure $next)
     {
-      $experiment_info = $this->clientConfigGetter->getExperimentConfig('defaultAcademicLevel');
+      $experiments = $this->clientConfigGetter->getExperimentsInfo();
+      $experiments_results = [];
+      $cookies_max_age = 10 * 365 * 24 * 60;
+      $cookie_string = "";
 
-      if (!$experiment_info['enabled']) {
-        return $next($request);
+      foreach ($experiments as $experiment_name => $experiment_info) {
+        switch ($experiment_name) {
+          case 'defaultAcademicLevel':
+            if (!$experiment_info['enabled']) {
+              break;
+            }
+      
+            $experiment_result_info = $this->defaultAcademicLevelExperiment->run($request, $experiment_info);
+      
+            $experiments_results['defaultAcademicLevelExperiment'] = [
+              'defaultAcademicLevel' => $experiment_result_info['academicLevelId'],
+              'defaultAcademicLevelExperimentGroup' => $experiment_result_info['experimentGroup']
+            ];
+
+            if (array_key_exists('cookie', $experiment_result_info)) {
+              list('name' => $name, 'value' => $value) = $experiment_result_info['cookie'];
+              $new_cookie = "$name=$value; Max-Age=$cookies_max_age";
+              $cookie_string = $cookie_string ? $cookie_string . ",$new_cookie" : $cookie_string . $new_cookie;
+            }
+            break;
+          case 'priceIncrease':
+            if (!$experiment_info['enabled']) {
+              break;
+            }
+
+            $experiment_result_info = $this->priceIncreaseExperiment->run($request, $experiment_info);
+
+            $experiments_results['priceIncreaseExperiment'] = [
+              'priceIncreaseValue' => $experiment_result_info['priceIncreaseValue'],
+              'priceIncreaseExperimentGroup' => $experiment_result_info['experimentGroup']
+            ];
+
+            if (array_key_exists('cookie', $experiment_result_info)) {
+              list('name' => $name, 'value' => $value) = $experiment_result_info['cookie'];
+              $new_cookie = "$name=$value; Max-Age=$cookies_max_age";
+              $cookie_string = $cookie_string ? $cookie_string . ",$new_cookie" : $cookie_string . $new_cookie;
+            }
+            break;
+          
+          default:
+            return $next($request);
+        }
       }
-
-      $experiment_result_info = $this->defaultAcademicLevelExperiment->run($request, $experiment_info);
-
-      $experiments_results = [
-        'defaultAcademicLevelExperiment' => [
-          'defaultAcademicLevel' => $experiment_result_info['academicLevelId'],
-          'defaultAcademicLevelExperimentGroup' => $experiment_result_info['experimentGroup']
-        ]
-      ];
 
       $request->attributes->add([
         'experimentsResults' => $experiments_results
       ]);
 
-      return array_key_exists('cookie', $experiment_result_info) 
-        ? $next($request)->cookie(
-          $experiment_result_info['cookie']['name'], 
-          $experiment_result_info['cookie']['value'], 
-          10 * 365 * 24 * 60, '/', null, false, false)
+      return $cookie_string 
+        ? $next($request)->header('Set-Cookie', $cookie_string)
         : $next($request);
     }
 }
--- src/Services/Experiments/PriceIncrease/PriceIncreaseExperiment.php
+++ src/Services/Experiments/PriceIncrease/PriceIncreaseExperiment.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Wizz\ApiClientHelpers\Services\Experiments\DefaultAcademicLevel;
+namespace Wizz\ApiClientHelpers\Services\Experiments\PriceIncrease;
 
 use Wizz\ApiClientHelpers\Helpers\CacheHelper;
 use Wizz\ApiClientHelpers\Helpers\CookieHelper;
 use Wizz\ApiClientHelpers\Services\Experiments\ExperimentTrait;
 use \Illuminate\Http\Request;
 
-class DefaultAcademicLevelExperiment
+class PriceIncreaseExperiment
 {
   use ExperimentTrait;
 
@@ -17,13 +17,24 @@ class DefaultAcademicLevelExperiment
     $experiment_groups = $experiment_info['groups'];
     $ab_group_from_cookie = $request->cookie($cookie_name);
 
+    $group_from_query = $request->query('priceIncreaseGroup');
+    if ($group_from_query) {
+      $info = $experiment_info['groups'][$group_from_query] ?? false;
+      if ($info) {
+        return [
+          'priceIncreaseValue' => $info['priceIncreaseValue'],
+          'experimentGroup' => $group_from_query
+        ];
+      }
+    }
+
     if (!$ab_group_from_cookie) {
       $weighted_values = $this->getWeightedValues($experiment_groups);
       $random_group_name = $this->getRandomWeightedGroup($weighted_values);
       $group_info = $experiment_groups[$random_group_name];
 
       return [
-        'academicLevelId' => $group_info['academicLevelId'],
+        'priceIncreaseValue' => $group_info['priceIncreaseValue'],
         'experimentGroup' => $random_group_name,
         'cookie' => [
           'name' => $cookie_name,
@@ -34,7 +45,7 @@ class DefaultAcademicLevelExperiment
 
     $group_info = $experiment_groups[$ab_group_from_cookie];
     return [
-      'academicLevelId' => $group_info['academicLevelId'],
+      'priceIncreaseValue' => $group_info['priceIncreaseValue'],
       'experimentGroup' => $ab_group_from_cookie
     ];
   }
