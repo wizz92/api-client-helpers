fa9209d8f35935f4e9e5570534cad18bb3e27704
liza <lizaqwerty.software@gmail.com>
2019-09-27 11:57:33 +0300

    add https

--- src/Services/AutoComposing/ScriptsCollector.php
+++ src/Services/AutoComposing/ScriptsCollector.php
@@ -96,7 +96,7 @@ class ScriptsCollector implements ComposingInterface
       */
     private function getValue(string $name, string $path): string
     {
-         $rootUrl = env('root_url', 'http://' . request()->getHttpHost());
+         $rootUrl = env('root_url', 'https://' . request()->getHttpHost());
 
          return app()->environment('local') ? "{$rootUrl}/{$name}" : "{$rootUrl}/{$path}";
     }
