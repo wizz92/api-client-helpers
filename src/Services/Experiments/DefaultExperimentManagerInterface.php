b752cf42d281d51b69dfc065a6f8242b777c9f7d
liza <lizaqwerty.software@gmail.com>
2020-01-16 12:28:47 +0200

    lil fix

--- src/Services/Experiments/DefaultExperimentManagerInterface.php
+++ src/Services/Experiments/DefaultExperimentManagerInterface.php
@@ -2,6 +2,8 @@
 
 namespace Wizz\ApiClientHelpers\Services\Experiments;
 
+use \Illuminate\Http\Request;
+
 interface DefaultExperimentManagerInterface
 {
     public function run(Request $request, array $experimentInfo, string $type);
