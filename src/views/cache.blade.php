aa5e0b2cd1b9140848a1e634e8558181700c46cf
Dmitriy Blashchyk <dmitriy.blashchyk@gmail.com>
2020-02-13 10:42:52 +0200

    LR-3392

--- src/views/cache.blade.php
+++ src/views/cache.blade.php
@@ -2,8 +2,9 @@
 <script type="text/javascript">
     window.onload = () => {
         window.addEventListener('message', (e) => {
+            const innerText = document.body.innerText
             if (e.data === 'response back') {
-                if (innerText.includes('success') || innerText.includes('was deleted')) {
+                if (innerText.includes('was deleted')) {
                     e.source.postMessage('restyle clicked button to success', '*')
                 } else {
                     e.source.postMessage('restyle clicked button to error', '*')
